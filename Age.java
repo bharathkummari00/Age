d39009e37163291d3a5d20ad02eb1ccdf72dd3a9
Bharath <bharathkummari544@gmail.com>
2025-01-30 17:36:08 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Age {
+    public static void main (String[] args ) {
+    Scanner sc = new Scanner(System.in);
+    int a=sc.nextInt();
+    System.out.println(2025-a);
+   
+
+
+    }
+    
+}
